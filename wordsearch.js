ce279f1b7adfea24ac0679ef57a4334d8c03587a
Asim <asim.awan@outlook.com>
2021-08-28 01:47:20 +0000

    final commit

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
+const transpose = function(matrix) {
+  let newMatrix = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    newMatrix.push(matrix.map(row => row[i]));
+  }
+  return newMatrix;
+};
+
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
+  if(letters.length===0){
+    return 'empty array'
+  }
+  const horizontalJoin = letters.map(ls => ls.join(''))
+  const verticleJoin=transpose(letters).map(col=>col.join(''))
+  for (l of horizontalJoin) {
+    // console.log(l)
+      if (l.includes(word)) return true
+      
+  }
+  for (row of verticleJoin){
+    if (row.includes(word)) return true
+  }
+
+  return false
 }
 
 module.exports = wordSearch
